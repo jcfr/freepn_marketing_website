4a326d3417ca7e5b33673d1b20a3ceec78aab771
Ian H. Bateman <ianhenrybateman@gmail.com>
2020-01-10 15:24:33 -0500

    finished artwork for landing page, saving all temp art

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@
     const sr = (window.sr = ScrollReveal())
 
     sr.reveal('.scroll_animate', {
-      duration: 1000,
+      duration: 1500,
       distance: '40px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       origin: 'bottom',
