4c0ddacc8b6317c58b9ee3673724f64783aa8e20
Ian H. Bateman <ianhenrybateman@gmail.com>
2020-01-21 10:35:35 -0300

    cleaned up some scripts in package.json, started on blog article generation script

--- scripts/build-blog-script.sh
+++ scripts/build-blog-script.sh
@@ -2,3 +2,46 @@
 
 # Script to generate the pages/blog.html page.
 # Generates article previews from md files in the /posts folder.
+
+# build header part one
+cat ./src/pages/blog_stubs/blog_header.txt > dist/pages/blog.html
+
+# add footer section
+cat ./src/pages/blog_stubs/blog_footer.txt >> dist/pages/blog.html
+
+echo "Built blog index file as blog.html successfully."
+
+
+# for f in ./src/posts/*.md; do
+# 	FILE=$(basename -- $f)
+# 	FILENAME="${FILE%.*}"
+# done
+
+
+# generate custom header tags
+# for i in {2..3}; do
+#   # get lines 2 and 3 from markdown file (TITLE and DESCRIPTION)
+#   line=$(head -n $i $f | tail -1)
+#   IFS=':' # colon (:) is set as delimiter
+#   read -ra ADDR <<< "$line" # str is read into an array as tokens separated by IFS
+#   IFS=' ' # reset to default value after usage
+
+#   value=$(echo "${ADDR[1]}" | sed 's/^[[:space:]]*//') # gets the value and trims leading whitespace
+#   tag=$(echo "${ADDR[0]}" | sed 's/^[[:space:]]*//') # gets the tag and trims leading whitespace
+
+#   if [ -z ${tag+x} ]; then 
+#     echo "Error: meta tag is unset. Please check your md file header section!"
+#   else
+#     case "$tag" in
+#       ("TITLE")
+#         echo "<title>FreePN - $value</title>" >> dist/posts/$FILENAME.html
+#       ;;
+#       ("DESCRIPTION")
+#         echo "<meta name="description" content="$value" />" >> dist/posts/$FILENAME.html
+#       ;;
+#       (*) 
+#         echo "Error: meta tag did not match any recognized value in file: $FILENAME. Please check your md file header section!"
+#       ;;
+#     esac
+#   fi
+# done
--- scripts/build-html-script.sh
+++ scripts/build-html-script.sh
@@ -3,4 +3,4 @@
 # Script to copy and build the html files and folders for distribution.
 
 # copy over html files
-cp src/*.html dist/ && mkdir -p dist/pages/ && cp -R src/pages/*.html dist/pages/
+cp src/*.html dist/ && mkdir -p dist/pages/ && cp -R src/pages/*.html dist/pages/ && ./scripts/build-blog-script.sh
