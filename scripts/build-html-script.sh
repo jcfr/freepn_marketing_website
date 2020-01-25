03c9e0c8e38873f1fe521fabf7399ee24cc1051e
Ian H. Bateman <ianhenrybateman@gmail.com>
2020-01-25 18:25:35 -0300

    cleaned up package.json scripts, added robot.txt, added blank sitemap file

--- scripts/build-html-script.sh
+++ scripts/build-html-script.sh
@@ -3,4 +3,4 @@
 # Script to copy and build the html files and folders for distribution.
 
 # copy over html files
-cp src/*.html dist/ && mkdir -p dist/pages/ && cp -R src/pages/*.html dist/pages/ && ./scripts/build-blog-script.sh
+cp src/CNAME dist/ && cp src/robots.txt dist/ && cp src/sitemap.xml dist/ && cp src/*.html dist/ && mkdir -p dist/pages/ && cp -R src/pages/*.html dist/pages/ && ./scripts/build-blog-script.sh
