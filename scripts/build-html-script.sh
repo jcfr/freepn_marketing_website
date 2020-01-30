40f4b8ef4d23bd99082c266bb0b8ca16ea769b19
Ian H. Bateman <ianhenrybateman@gmail.com>
2020-01-29 22:30:15 -0300

    minor fix to html build script

--- scripts/build-html-script.sh
+++ scripts/build-html-script.sh
@@ -3,5 +3,5 @@
 # Script to copy and build the html files and folders for distribution.
 
 # copy over html, robots.txt, and sitemap.xml files
-cp src/CNAME dist/ && mkdir -p dist/downloads/ && cp src/downloads/* dist/downloads/ && cp src/robots.txt dist/ && cp src/*.html dist/ && mkdir -p dist/pages/ && cp -R src/pages/*.html dist/pages/ && ./scripts/build-blog-script.sh && ./scripts/build-sitemap-script.sh
+cp src/CNAME dist/ && rm -rf dist/downloads/ && mkdir -p dist/downloads/ && cp src/downloads/* dist/downloads/ && cp src/robots.txt dist/ && cp src/*.html dist/ && mkdir -p dist/pages/ && cp -R src/pages/*.html dist/pages/ && ./scripts/build-blog-script.sh && ./scripts/build-sitemap-script.sh
 echo 'Built all HTML files successfully.'
